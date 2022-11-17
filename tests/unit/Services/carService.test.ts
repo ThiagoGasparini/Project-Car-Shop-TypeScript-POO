c076df6bee51a7cecb327ac6f2c4b66b31d373f7
Thiago Gasparini <thiago_engprod@outlook.com>
2022-11-17 18:56:17 -0300

    add requisito 03

---
+++ tests/unit/Services/carService.test.ts
@@ -0,0 +1,65 @@
+import sinon from 'sinon';
+import { expect } from 'chai';
+import { Model } from 'mongoose';
+import CarService from '../../../src/Services/CarService';
+import { carMock, carMockWithId, carsOutput, carDomain, carList } from './mocks/carMocks';
+
+const ERROR_CAR = 'Car not found';
+
+describe('Car Service', function () {
+  afterEach(function () {
+    sinon.restore();
+  });
+
+  describe('When creating a car', function () {
+    it('Creates a new car succesfully', async function () {
+      sinon.stub(Model, 'create').resolves(carMockWithId);
+
+      const service = new CarService();
+      const result = await service.create(carMock);
+      expect(result).to.be.deep.equal(carMockWithId);
+    });
+  });
+
+  describe('When searching all cars', function () {
+    it('Return all cars succesfully', async function () {
+      sinon.stub(Model, 'find').resolves(carsOutput);
+
+      const service = new CarService();
+      const result = await service.getAll();
+      expect(result).to.be.deep.equal(carList);
+    });
+    it('Creates a new car failed', async function () {
+      sinon.stub(Model, 'find').resolves();
+
+      const service = new CarService();
+      const result = await service.getAll();
+  
+      expect(result).to.be.deep.equal([]);
+    });
+  });
+
+  describe('When searching a car', function () {
+    it('Return a car succesfully', async function () {
+      const id = '6352f8ea74092b2e6a784c51';
+      sinon.stub(Model, 'findOne').resolves(carMockWithId);
+
+      const service = new CarService();
+      const result = await service.findById(id);
+
+      expect(result).to.be.deep.equal(carDomain);
+    });
+
+    it('Fails to return a car', async function () {
+      const id = '6352f8ea74092b2e6a784c51';
+      sinon.stub(Model, 'findOne').resolves();
+
+      try {
+        const service = new CarService();
+        await service.findById(id);
+      } catch (error) {
+        expect((error as Error).message).to.be.equal(ERROR_CAR);
+      }
+    });
+  });
+});
